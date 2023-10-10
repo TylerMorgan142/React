fba2c7c7e906a32d5f59c6e050197de89a86e6b8
TylerMorgan142 <tylermorgan142@gmail.com>
2023-10-10 02:52:12 +0000

    Added EventsClass and EventsFunctional handlers

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import StatefulGreetingWithPrevState from "./components/StatefulGreetingWithPrevState"
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
-  return (
-    <div className="App">
-      <StatefulGreetingWithPrevState />
-    </div>
-  );
+  return <div className="App">
+    <EventsFunctional />
+    <EventsClass />
+  </div>;
 }
 
 export default App;
