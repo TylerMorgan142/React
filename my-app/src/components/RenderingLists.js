e1b1c838a1c693ae6ffb6c6ccf210b83d568ad22
TylerMorgan142 <tylermorgan142@gmail.com>
2023-10-12 04:00:57 +0000

    Added keys to book list

--- my-app/src/components/RenderingLists.js
+++ my-app/src/components/RenderingLists.js
@@ -28,7 +28,7 @@ function RenderingLists() {
     return(
         <div>
             {bookList.map(book => {
-                return <h2>{book}</h2>
+                return <h2 key={book}>{book}</h2>
             })}
             <hr />
             {
@@ -44,7 +44,7 @@ function RenderingLists() {
             <hr />
             {
                 books.map(book => {
-                    return <Book book={book} />
+                    return <Book key={book.title} book={book} />
                 })
             }
         </div>
